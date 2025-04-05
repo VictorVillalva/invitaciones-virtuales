b4e799995a2b128a91d02a2696cda5cfe2b69a66
Victor Villalva <victor.villalva@sdnet.com.mx>
2025-04-04 23:52:14 -0600

    Implementacion de todo dentro de la page que se renderiza

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
+import Premium from "@/components/pages/premium";
+
 export default function Home() {
   return (
-    <div className="w-full h-120 flex flex-row justify-center items-center">
-      <h1 className="font-extrabold text-9xl text-center">Invitaciones Virtuales</h1>
-    </div>
+    <Premium />
   );
 }
