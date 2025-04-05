2bcb8eb4cc805ea403db85b1f1a7cd8ee491899c
Victor Villalva <victor.villalva@sdnet.com.mx>
2025-04-04 23:51:59 -0600

    Agregacion de separador entre secciones

---
+++ src/components/pages/premium.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import separador from "@/assets/images/DanielaMedelXV/Separador.png"
+
+import ApartaFecha from "../ui/ApartaFecha";
+import Header from "../ui/Header";
+import SobreAnimation from "../ui/SobreAnimation";
+
+
+export default function Premium() {
+    const COLORTEXT = '#323C5D'
+    return (
+        <>
+            <SobreAnimation>
+                <Header />
+                <main className="w-full px-4 py-[100px] relative z-10">
+                    <div className={`flex flex-col items-center text-[${COLORTEXT}] gap-[120]`}>
+                        <section className='ApartaFecha'>
+                            <ApartaFecha />
+                        </section>
+                        <div className="separador flex" data-aos="fade-up">
+                            <Image src={separador} alt="separador" />
+                        </div>
+                    </div>
+                </main>
+            </SobreAnimation>
+        </>
+    )
+}
