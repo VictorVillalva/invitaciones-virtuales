39ed841a28dda120a10afee9d9bd526d9bc77491
vvillalva <victor.villalva0326@gmail.com>
2025-07-09 10:40:41 -0600

    Fix: Cambio de codigo de vestimenta (Etiqueta -> Formal)

--- src/components/components-invitations/CodigoVestimenta.tsx
+++ src/components/components-invitations/CodigoVestimenta.tsx
@@ -7,7 +7,7 @@ export default function CodigoVestimenta() {
         <div className="Codigo-vestimenta flex flex-col gap-6" data-aos="fade-up">
             <p className="text-center text-[32px]">Codigo de vestimenta</p>
             <div className={`${quicksand.className} text-center text-xl flex flex-col items-center`}>
-                <p>Etiqueta</p>
+                <p>Formal</p>
                 <Image src={etiqueta} alt=" " width={200} />
             </div>
         </div> 
