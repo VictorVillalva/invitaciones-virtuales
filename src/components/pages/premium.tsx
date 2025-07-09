4ed5266f9a1abb04a7e9097875002bec5d2ea461
vvillalva <victor.villalva0326@gmail.com>
2025-07-09 15:05:12 -0600

    FEAT: eliminación de seccion de asistencia, creacion de vista aparte para confirmacion de asistencia

--- src/components/pages/premium.tsx
+++ src/components/pages/premium.tsx
@@ -5,7 +5,7 @@ import LogoDanielMedel from "@/assets/images/DanielaMedelXV/Logo_DanielMedel.jpg
 //Data
 import { Bancos, nombrePadresPadrinos } from '@/assets/data/DanielaMedel/db'
 //Fonts
-import { inspiration, playwrite } from '@/assets/fonts/fonts'
+import { inspiration, playwrite, quicksand } from '@/assets/fonts/fonts'
 //Components
 import Image from 'next/image'
 import Header from '../components-invitations/Header'
@@ -21,13 +21,15 @@ import Ubicacion from '../components-invitations/Ubicacion'
 import Hoteles from '../components-invitations/Hoteles'
 import BackgroundMusic from '../components-invitations/BackgroundMusic'
 import Footer from '../components-invitations/Footer'
-import Asistencia from '../components-invitations/Asistencia'
 import Playlist from "../components-invitations/Playlist"
 import GaleriaFotos from "../components-invitations/GaleriaFotos"
+import CountDown from "../components-invitations/CountDown"
+import Link from "next/link"
 
 export default function Premium() {
     const COLORTEXT = '#323C5D'
-    
+    const fechaEvento = new Date('2025-12-31T00:00:00');
+
     return (
         <>
             <SobreAnimation>
@@ -110,11 +112,22 @@ export default function Premium() {
                             <Image src={separador} alt="separador" />
                         </div>
                         <section className="confirmacionAsistencia">
-                            <Asistencia />
+                            <div className="asistencia flex flex-col gap-10" data-aos="fade-up">
+                                <p className="text-[32px] text-center">Confirmación de asistencia</p>
+                                <CountDown targetDate={fechaEvento} />
+                                <div className="flex flex-col gap-4">
+                                    <small className={`${quicksand.className} text-center`}>Da click en el boton para confirmar tu asistencia al evento</small>
+                                    <Link 
+                                        href="/daniela-medel/confirmacion" 
+                                        className={`${quicksand.className} w-full py-2 bg-[#CBA836] text-white rounded hover:bg-[#d9b12d] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#927823] font-semibold text-center`}>
+                                            Confirmar asistencia
+                                    </Link>
+                                </div>
+                            </div>
                         </section>
                         <div className="footer flex flex-col justify-center items-center gap-6" >
                             <div className="relative">
-                                <Image src={LogoDanielMedel} alt="Logo" width={60}/>  
+                                <Image src={LogoDanielMedel} alt="Logo" width={60} />
                             </div>
                             <div className="flex flex-col">
                                 <p className={`${inspiration.className} text-[96px] text-center leading-none`}>Mis XV</p>
