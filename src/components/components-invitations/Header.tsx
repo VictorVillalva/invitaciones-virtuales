f8abeea5d48a7bd333b5cefa7e20ee1f805138ef
vvillalva <victor.villalva0326@gmail.com>
2025-07-09 11:54:25 -0600

    Fix: Cambio de foto de header

--- src/components/components-invitations/Header.tsx
+++ src/components/components-invitations/Header.tsx
@@ -1,20 +1,28 @@
 import Image from 'next/image'
 import { inspiration } from '@/assets/fonts/fonts'
-import fotoPrueba from '@/assets/images/DanielaMedelXV/foto-header-DanielaMedel.png'
-
+import DanielaMedel from '@/assets/images/DanielaMedelXV/VIL_0316.webp'
 export default function Header() {
     return (
         <header>
-            <div className='relative w-full h-auto flex flex-col items-center pt-12'>
+            <div className='relative w-full h-[900px] max-w-[768px] mx-auto flex flex-col justify-center items-center'>
                 <Image 
-                    src={fotoPrueba}
+                    src={DanielaMedel}
                     alt='Daniela Medel Quinceañera'
-                    className='mask-fade-bottom'
+                    fill
+                    className='mask-fade-bottom object-cover'
                     priority
                 />
-                <div className="absolute bottom-10 flex flex-col items-center justify-end mb-24 text-[#323C5D]">
-                    <span className={`${inspiration.className} text-[60px] leading-none`}>Mis XV</span>
-                    <span className="text-[16px] font-normal">Daniela Medel Muñiz</span>
+                <div className="absolute bottom-10 flex flex-col items-center justify-end mb-24 text-white gap-2">
+                    {/* <Image 
+                        src={LogoDanielaMedel}
+                        alt=' '
+                        width={40}
+                        className="filter invert brightness-0 contrast-200"
+                    /> */}
+                    <div className='flex flex-col items-center'>
+                        <span className={`${inspiration.className} text-[60px] leading-none`}>Mis XV</span>
+                        <span className="text-[16px] font-normal">Daniela Medel Muñiz</span>
+                    </div>
                 </div>
             </div>
         </header>
