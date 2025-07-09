e079784e6534f4b93199b168b56ca6653a011aca
vvillalva <victor.villalva0326@gmail.com>
2025-07-09 10:29:39 -0600

    Feat: Implementación de bloqueo de vista para dispositivos mayores a 768px la pantalla

--- src/app/daniela-medel/page.tsx
+++ src/app/daniela-medel/page.tsx
@@ -1,7 +1,15 @@
+'use client'
+import BlockMobile from "@/components/components-invitations/BlockMobile";
 import Premium from "@/components/pages/premium";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 export default function DanielaMedel() {
+    const { isMobile } = useIsMobile()
+
     return (
-        <Premium />
+        <>
+            {!isMobile && <BlockMobile />}
+            <Premium />
+        </>
     );
 }
---
+++ src/components/components-invitations/BlockMobile.tsx
@@ -0,0 +1,27 @@
+'use client'
+import { inter } from "@/assets/fonts/fonts";
+import { useEffect } from "react";
+import LogoDanielaMedel from "@/assets/images/DanielaMedelXV/Logo_DanielMedel.jpg"
+import Image from 'next/image'
+import Background from "./Background";
+
+export default function BlockMobile() {
+    useEffect(() => {
+        // Guarda el valor original
+        const original = document.body.style.overflow;
+        // Bloquea el scroll
+        document.body.style.overflow = "hidden";
+        // Al desmontar, restáuralo
+        return () => { document.body.style.overflow = original; };
+    }, []);
+    return (
+        <div className={`block-responsive ${inter.className} antialiased fixed inset-0 z-[10000] bg-white text-primary flex flex-col items-center justify-center text-2xl text-center gap-6`}>
+            <Background />
+            <Image src={LogoDanielaMedel} alt={"Logo"} width={100} height={100} />
+            <div className="flex flex-col">
+                <p className="font-bold text-4xl">Usa un dispositivo móvil</p>
+                <small>Esta funcionalidad solo está disponible en pantallas móviles.</small>
+            </div>
+        </div>
+    )
+}
