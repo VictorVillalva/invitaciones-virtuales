1215bb00e068720044f9c8d7aec724a904486447
vvillalva <victor.villalva0326@gmail.com>
2025-07-09 15:20:27 -0600

    Feat: Configuracion de la nueva vista de confirmacion

--- src/app/daniela-medel/confirmacion/page.tsx
+++ src/app/daniela-medel/confirmacion/page.tsx
@@ -1,5 +1,26 @@
-export default function Confirmacion() {
+'use client'
+import Confirmacion from "@/components/components-invitations/Confirmacion";
+import Footer from "@/components/components-invitations/Footer";
+import Invitacion from "@/components/components-invitations/Invitacion";
+
+export default function ConfirmacionAsistencia() {
+  const COLORTEXT = '#323C5D'
+
   return (
-    <div>Hola mundo</div>
+    <>
+      <main className={`text-[${COLORTEXT}] w-full px-4 py-[100px] relative z-10`}>
+        <div className="asistencia flex flex-col gap-10" data-aos="fade-up">
+          <p className="text-[32px] text-center">Confirmación de asistencia</p>
+          <Invitacion
+            numPersonas={3}
+            familia="Muñiz"
+            adultos={2}
+            ninos={1}
+          />
+          <Confirmacion />
+        </div>
+      </main>
+      <Footer />
+    </>
   )
 }
