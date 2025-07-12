e5d274d109751db6a03ce8cc40f837c3c213c2bb
VictorVillalva <victor.villalva2003@hotmail.com>
2025-07-12 16:27:48 -0600

    Fix: Corregir la hora del evento "Baile" en el componente db.ts

--- src/assets/data/DanielaMedel/db.ts
+++ src/assets/data/DanielaMedel/db.ts
@@ -29,7 +29,7 @@ export const ImagesCarrusel = [
 export const evento = [
     { title: "Iglesia", time: "17:00 pm", icon: iglesia },
     { title: "Recepción", time: "18:30 pm", icon: bell },
-    { title: "Baile", time: "17:00 pm", icon: dance },
+    { title: "Baile", time: "19:00 pm", icon: dance },
     { title: "Cena", time: "20:00 pm", icon: cena },
     { title: "Fiesta", time: "21:00 pm", icon: party },
 ];
