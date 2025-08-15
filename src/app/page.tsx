9baec2b58f4332ca0f85dbe1dec4a5a79871efe4
VictorVillalva <victor.villalva2003@hotmail.com>
2025-08-14 22:46:32 -0600

    Feat: Implementacion de seccion de Proceso y Paquetes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 //** Images-Assests */
 import Rabe from "@/assets/images/landing/rabe-logo.png";
 import headerImage from "@/assets/images/landing/image-header-1.png";
+import processImage from "@/assets/images/landing/image-proceso-1.png";
+import paquete1 from "@/assets/images/landing/image-paquetes-1.png";
+import paquete2 from "@/assets/images/landing/image-paquetes-2.png";
+import paquete3 from "@/assets/images/landing/image-paquetes-3.png";
 import { ArrowUpRight } from "lucide-react";
 //** Components */
 import Image from "next/image";
@@ -101,7 +105,7 @@ export default function Home() {
         </div>
       </section>
       <section className="beneficios-landing-page bg-white ">
-        <div className="encabezado lg:px-[134px] px-4  flex flex-col justify-center items-center gap-6 mt-[100px]">
+        <div className="encabezado lg:px-[134px] px-4 flex flex-col justify-center items-center gap-6 mt-[100px]">
           <BadgeRabe variant="outline" className="font-semibold">
             BENEFICIOS
           </BadgeRabe>
@@ -120,23 +124,103 @@ export default function Home() {
         </div>
       </section>
       <section className="proceso-landing-page bg-rabe-primary-100 py-[50px] mt-[100px]">
-        <div className="encabezado lg:px-[134px] px-4  flex flex-col justify-center items-center gap-6 mt-[100px]">
-          <BadgeRabe variant="outline" className="font-semibold">
-            BENEFICIOS
-          </BadgeRabe>
-          <div className="encabezado lg:px-[236px] flex flex-col items-center text-center gap-2.5">
-            <h2 className="font-semibold tracking-[-0.08em] text-4xl text-rabe-primary">
-              Porque tu evento merece lo mejor desde la invitación
-            </h2>
-            <p className="font-medium tracking-[-0.08em] text-xs text-neutral-600">
-              No es solo una invitación, es la primera experiencia que tendrán
-              tus invitados de tu evento. Con nosotros, cada detalle cuenta:
-              diseños únicos, entrega rápida y la magia de lo digital para
-              sorprender y emocionar desde el primer clic.
-            </p>
+        <div className="encabezado lg:px-[134px] px-4 flex flex-col justify-center items-center gap-10">
+          <div className="encabezado-titulos flex flex-col justify-center items-center gap-6">
+            <BadgeRabe variant="outline" className="font-semibold">
+              PROCESO
+            </BadgeRabe>
+            <div className="encabezado lg:px-[236px] flex flex-col items-center text-center gap-2.5">
+              <h2 className="font-semibold tracking-[-0.08em] text-4xl text-rabe-primary">
+                Porque tu evento merece lo mejor desde la invitación
+              </h2>
+              <p className="font-medium tracking-[-0.08em] text-xs text-neutral-600">
+                No es solo una invitación, es la primera experiencia que tendrán
+                tus invitados de tu evento. Con nosotros, cada detalle cuenta:
+                diseños únicos, entrega rápida y la magia de lo digital para
+                sorprender y emocionar desde el primer clic.
+              </p>
+            </div>
+          </div>
+          <div className="pasos flex flex-row gap-4">
+            <div className="pasos-descripcion flex flex-col gap-6">
+              <div className="paso flex flex-col gap-0 tracking-[-0.08em]">
+                <h3 className="text-rabe-primary-500 font-semibold">Paso 1:  Elige tu estilo y personaliza</h3>
+                <p className="text-xs text-neutral-500">Explora nuestra colección de diseños y selecciona el que mejor represente tu evento. Luego, personalízalo con tus colores, textos, fotos y detalles especiales para que sea 100% único.</p>
+              </div>
+              <div className="paso flex flex-col gap-0 tracking-[-0.08em]">
+                <h3 className="text-rabe-primary-500 font-semibold">Paso 2: Revisa y aprueba el diseño</h3>
+                <p className="text-xs text-neutral-500">Te enviamos una vista previa para que revises cada detalle. Podrás solicitar cambios hasta que quede tal como lo imaginaste.</p>
+              </div>
+              <div className="paso flex flex-col gap-0 tracking-[-0.08em]">
+                <h3 className="text-rabe-primary-500 font-semibold">Paso 3: Recibe tu invitación digital</h3>
+                <p className="text-xs text-neutral-500">Obtendrás tu invitación lista para compartir, optimizada para verse perfecta en cualquier dispositivo, desde celulares hasta computadoras.</p>
+              </div>
+              <div className="paso flex flex-col gap-0 tracking-[-0.08em]">
+                <h3 className="text-rabe-primary-500 font-semibold">Paso 4: Envía y recibe confirmaciones</h3>
+                <p className="text-xs text-neutral-500">Comparte tu invitación por WhatsApp, redes sociales o correo. Tus invitados podrán confirmar su asistencia con un solo clic y tú podrás llevar un registro en tiempo real.</p>
+              </div>
+              <div className="paso flex flex-col gap-0 tracking-[-0.08em]">
+                <h3 className="text-rabe-primary-500 font-semibold">Paso 1:  Elige tu estilo y personaliza</h3>
+                <p className="text-xs text-neutral-500">Explora nuestra colección de diseños y selecciona el que mejor represente tu evento. Luego, personalízalo con tus colores, textos, fotos y detalles especiales para que sea 100% único.</p>
+              </div>
+            </div>
+            <Image
+              src={processImage}
+              alt="Rabe Innovation"
+              width={568}
+              height={329}
+              className="object-contain"
+            />
           </div>
         </div>
       </section>
+      <section className="paquetes-landing-page bg-white mb-[1000px] flex flex-col gap-[60px]">
+        <div className="encabezado lg:px-[134px] px-4 flex flex-col justify-center items-center gap-6 mt-[100px]">
+          <BadgeRabe variant="outline" className="font-semibold">
+            PAQUETES
+          </BadgeRabe>
+          <div className="encabezado lg:px-[236px] flex flex-col items-center text-center gap-2.5">
+            <h2 className="font-semibold tracking-[-0.08em] text-4xl text-rabe-primary">
+              Checa los diferentes paquetes que tenemos para ti
+            </h2>
+            <p className="font-medium tracking-[-0.08em] text-xs text-neutral-600">
+              Encuentra la opción ideal para tu evento. Desde diseños básicos hasta experiencias digitales premium, tenemos un plan que se adapta a tu estilo, tus necesidades y tu presupuesto.
+            </p>
+          </div>
+        </div>
+        <div className="paquetes flex flex-col gap-36">
+          <article className="paquete-core flex flex-row justify-end gap-4 lg:pl-[134px]">
+            <div className="descripcion-paquete">
+              hola
+            </div>
+            <Image
+              src={paquete1}
+              alt="Invitación Core"
+              className="object-cover relative max-w-[712px] h-[452px] overflow-hidden rounded-l-[9999px]"
+            />
+          </article>
+          <article className="paquete-rise flex flex-row gap-4 lg:pr-[134px]">
+            <Image
+              src={paquete2}
+              alt="Invitación Rise"
+              className="object-cover relative max-w-[712px] h-[452px] overflow-hidden rounded-r-[9999px]"
+            />
+            <div className="descripcion-paquete">
+              hola
+            </div>
+          </article>
+          <article className="paquete-core flex flex-row justify-end gap-4 lg:pl-[134px]">
+            <div className="descripcion-paquete">
+              hola
+            </div>
+            <Image
+              src={paquete3}
+              alt="Invitación Nova"
+              className="object-cover relative max-w-[712px] h-[452px] overflow-hidden rounded-l-[9999px]"
+            />
+          </article>
+        </div>
+      </section>z
     </main>
   );
 }
