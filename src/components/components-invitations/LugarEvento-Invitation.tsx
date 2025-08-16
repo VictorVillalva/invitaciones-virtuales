03caaf19334f6c79e97728b9ecf738db751739b6
VictorVillalva <victor.villalva2003@hotmail.com>
2025-08-16 14:05:23 -0600

    Feat: Creación del componente CardLugarEvento para mostrar la información del lugar del evento

---
+++ src/components/components-invitations/LugarEvento-Invitation.tsx
@@ -0,0 +1,41 @@
+//** Hooks */}
+import { StaticImageData } from 'next/image'
+//** Components */
+import CardLugarEvento from '@/components/components-invitations/CardLugarEvento-Invitation-1'
+import { url } from 'inspector';
+//** Interfaces or Types*/
+interface LugareEventoProps {
+    title: string;
+    classNameTitle?: string;
+    titleCard: string;
+    classNameTitleCard?: string;
+    image: StaticImageData;
+    location: string;
+    classNameButton?: string;
+    urlLocation: string;
+}
+export default function LugarEventoInvitation({ 
+    title = "Lugar del Evento", 
+    classNameTitle, 
+    titleCard, 
+    classNameTitleCard, 
+    image, 
+    location,
+    classNameButton,
+    urlLocation = "#"
+}: LugareEventoProps) {
+
+    return (
+        <div className="flex flex-col gap-6">
+            <h2 className={`${classNameTitle}`}>{title}</h2>
+            <CardLugarEvento
+                image={image}
+                location={location}
+                title={titleCard}
+                url={urlLocation}
+                classNameTitle={classNameTitleCard}
+                classNameButton={classNameButton}
+            />
+        </div>
+    )
+}
