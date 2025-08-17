8e2319f93b11b27fc50f344b81febb7519dd22a2
VictorVillalva <victor.villalva2003@hotmail.com>
2025-08-16 19:28:48 -0600

    feat: Agregar AOS para animaciones y actualizar dependencias en package.json

--- src/app/misXV/pamela-castillo/layout.tsx
+++ src/app/misXV/pamela-castillo/layout.tsx
@@ -1,6 +1,9 @@
+//** Assets */
 import { zain } from "@/assets/fonts/fonts"
 import "../../globals.css";
 import bgImage from "@/assets/images/PamelaCastilloXV/Background-PamelaCastillo.png"
+//** Hooks */
+import AOSInit from "@/components/components-invitations/AOSInit";
 
 //Nota: El body solo se modifica en el layout de la app, no en el de la pagina
 export default function DanielaLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
@@ -9,6 +12,7 @@ export default function DanielaLayout({ children }: Readonly<{ children: React.R
             <section className={`pamela-castillo-layout ${zain.className} antialiased relative`}>
                 <div className="absolute inset-0 h-auto" style={{ backgroundImage: `url(${bgImage.src})` }}></div>
                 <div className="relative z-10">
+                    <AOSInit />
                     {children}
                 </div>
             </section>
--- src/app/misXV/pamela-castillo/page.tsx
+++ src/app/misXV/pamela-castillo/page.tsx
@@ -2,14 +2,12 @@
 //** Hooks */}
 import Image from 'next/image'
 import { useIsMobile } from '@/hooks/useIsMobile'
-
 //** Components */
 import HeaderInvitation from '@/components/components-invitations/Header-Invitation'
 import ApartaFechaInvitation from '@/components/components-invitations/ApartaFecha-Invitation'
 import TextoInspirador from '@/components/components-invitations/TextoInspirador-Invitation'
 import CountDown from '@/components/components-invitations/CountDown-Invitation'
 import LugarEvento from '@/components/components-invitations/LugarEvento-Invitation'
-
 //** Assets */
 import PamelaCastillo from '@/assets/images/PamelaCastilloXV/Portada-PamelaCastillo.png'
 import Lugar from '@/assets/images/PamelaCastilloXV/Hacienda-ElJaguey.png'
@@ -17,8 +15,6 @@ import separador from '@/assets/images/PamelaCastilloXV/Separator-8-PamelaCastil
 import { ephesis } from '@/assets/fonts/fonts'
 import BlockMobile from '@/components/components-invitations/BlockMobile-Invitation'
 import FooterRabe from '@/components/components-invitations/Footer-Rabe'
-
-
 
 export default function PamelaCastilloPreInvitation() {
   const { isMobile } = useIsMobile()
@@ -34,41 +30,59 @@ export default function PamelaCastilloPreInvitation() {
         classNameSubtitle='text-[20px]'
       />
       <main className='pre-invitacion-pamela-castillo flex flex-col gap-[150px] px-4 py-[150px]'>
-        <ApartaFechaInvitation
-          date='8 de Noviembre del 2025'
-          classNameTitle='tracking-[0.16em] text-pamela-primary font-normal'
-          classNameText='text-[40px] font-bold tracking-[-0.02em] text-[#C4870A]'
-        />
-        <Image src={separador} alt="separador" />
-        <TextoInspirador
-          title='La vida está hecha de instantes únicos, y este es uno de ellos. Me encantaría que me acompañes a celebrar mis XV años y a compartir juntos este día tan especial.'
-          classNameText='text-[24px] font-normal text-[#814D12]'
-        />
-        <Image src={separador} alt="separador" />
-        <CountDown
-          targetDate={new Date('2025-11-08T00:00:00')}
-          classNameTitle='text-center text-[40px] font-bold text-pamela-primary tracking-[-0.02em] leading-[40px]'
-        />
-        <Image src={separador} alt="separador" />
-        <LugarEvento
-          title='Lugar del Evento'
-          classNameTitle='text-center text-[40px] font-bold text-pamela-primary tracking-[-0.02em] leading-[40px]'
-          image={Lugar}
-          location='Prolongación Hacienda El Jaguey 3402 Cholula, 72828 Heroica Puebla de Zaragoza'
-          urlLocation='https://maps.app.goo.gl/bnV6uGG2rLP7YQYWA'
-          titleCard='Hacienda El Jaguey'
-          classNameTitleCard='text-[32px] tracking-[-0.06em] text-pamela-primary'
-          classNameButton='rounded-[16px] text-[18px] text-white bg-[linear-gradient(to_right,#435A62_0%,#668995_34%,#77A0AF_68%,#89B8C8_100%)]'
-        />
-        <Image src={separador} alt="separador" />
-        <section className='Espera-la-invitación'>
+        <section data-aos="fade-dowm" className='aparta-fechas-invitation'>
+          <ApartaFechaInvitation
+            date='8 de Noviembre del 2025'
+            classNameTitle='tracking-[0.16em] text-pamela-primary font-normal'
+            classNameText='text-[40px] font-bold tracking-[-0.02em] text-[#C4870A]'
+          />
+        </section>
+        <section data-aos="fade-dowm" className='separador-invitation'>
+          <Image src={separador} alt="separador" />
+        </section>
+        <section data-aos="fade-dowm" className='texto-inspirador-invitation'>
+          <TextoInspirador
+            title='La vida está hecha de instantes únicos, y este es uno de ellos. Me encantaría que me acompañes a celebrar mis XV años y a compartir juntos este día tan especial.'
+            classNameText='text-[24px] font-normal text-[#814D12]'
+          />
+        </section>
+        <section data-aos="fade-dowm" className='separador-invitation'>
+          <Image src={separador} alt="separador" />
+        </section>
+        <section data-aos="fade-dowm" className='countdown-invitation'>
+          <CountDown
+            targetDate={new Date('2025-11-08T00:00:00')}
+            classNameTitle='text-center text-[40px] font-bold text-pamela-primary tracking-[-0.02em] leading-[40px]'
+          />
+        </section>
+        <section data-aos="fade-dowm" className='separador-invitation'>
+          <Image src={separador} alt="separador" />
+        </section>
+        <section data-aos="fade-dowm" className='lugar-evento-invitation'>
+          <LugarEvento
+            title='Lugar del Evento'
+            classNameTitle='text-center text-[40px] font-bold text-pamela-primary tracking-[-0.02em] leading-[40px]'
+            image={Lugar}
+            location='Prolongación Hacienda El Jaguey 3402 Cholula, 72828 Heroica Puebla de Zaragoza'
+            urlLocation='https://maps.app.goo.gl/bnV6uGG2rLP7YQYWA'
+            titleCard='Hacienda El Jaguey'
+            classNameTitleCard='text-[32px] tracking-[-0.06em] text-pamela-primary'
+            classNameButton='rounded-[16px] text-[18px] text-white bg-[linear-gradient(to_right,#435A62_0%,#668995_34%,#77A0AF_68%,#89B8C8_100%)]'
+          />
+        </section>
+        <section data-aos="fade-dowm" className='separador-invitation'>
+          <Image src={separador} alt="separador" />
+        </section>
+        <section data-aos="fade-dowm" className='Espera-la-invitación'>
           <div className='flex flex-col gap-3 text-center px-4 text-pamela-primary'>
             <h2 className=' font-bold text-[40px] tracking-[-0.06em] leading-[40px]'>Espera la invitación oficial</h2>
             <p className='tracking-[-0.02em]'>Estamos trabajando en todos los detalles para que este evento sea especial, en los próximos días estarás recibiendo la información oficial para este gran evento</p>
           </div>
         </section>
-        <Image src={separador} alt="separador" />
-        <section className='misXV'>
+        <section data-aos="fade-dowm" className='separador-invitation'>
+          <Image src={separador} alt="separador" />
+        </section>
+        <section data-aos="fade-dowm" className='misXV'>
           <div className='flex flex-col justify-center items-center text-center gap-2'>
             <span className={`${ephesis.className} text-[80px] leading-none text-[#C4870A]`}>Mis XV</span>
             <span className={`text-[20px] font-bold text-[#9C610C]`}>Pamela Castillo Monterrosas</span>
